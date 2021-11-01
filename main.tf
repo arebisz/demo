eed7f1d7ce18d315d3e082679e8ea2d73c114d46
amwr91 <amwr91@gmail.com>
2021-11-01 22:26:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ provider "google" {
 }
 
 module "monitoring" {
-    source = "./monitoring"
-    count = var.include_monitoring_module ? 1 : 0
+    count = var.monitoring ? 1 : 0
+    source = "./monitoring
     email_address = var.email_address
     email_display_name = var.email_display_name
 }
