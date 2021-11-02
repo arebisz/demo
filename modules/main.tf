2c66143f9eeef8b749f758378fa3c38eb4158896
amwr91 <amwr91@gmail.com>
2021-11-02 17:56:16 +0100

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,9 +1,9 @@
 locals {
-  cache_path           = local.skip_download ? "" : "${path.module}/cache/${random_id.cache[0].hex}"
+  cache_path           = local.skip_download ? "" : "./cache/${random_id.cache[0].hex}"
   gcloud_bin_path      = "${local.cache_path}/google-cloud-sdk/bin"
   components           = join(",", var.additional_components)
   gcloud              = local.skip_download ? "gcloud" : "${local.gcloud_bin_path}/gcloud"
-  additional_components_command                = "${path.module}/scripts/check_components.sh ${local.gcloud} ${local.components}"
+  additional_components_command                = "./scripts/check_components.sh ${local.gcloud} ${local.components}"
   download_override = var.enabled ? data.external.env_override[0].result.download : ""
   skip_download     = local.download_override == "always" ? false : (local.download_override == "never" ? true : var.skip_download)
 }
@@ -45,6 +45,6 @@ resource "random_id" "cache" {
 data "external" "env_override" {
   count = var.enabled ? 1 : 0
 
-  program = ["${path.module}/scripts/check_env.sh"]
+  program = [".//scripts/check_env.sh"]
   query   = {}
 }  
