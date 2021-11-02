83ccae4038482575dd4c2d6e09cc4769412e9481
amwr91 <amwr91@gmail.com>
2021-11-02 14:28:22 +0100

    Update logging.tf

--- logging/logging.tf
+++ logging/logging.tf
@@ -6,6 +6,22 @@ resource "google_storage_bucket" "cos-logging-bucket" {
   }
 }
 
+module "agent_policy" {
+  source     = "terraform-google-modules/cloud-operations/google//modules/agent-policy"
+  version    = "~> 0.1.0"
+
+  project_id = "<PROJECT ID>"
+  policy_id  = "ops-agents-example-policy"
+  agent_rules = [
+    {
+      type               = "ops-agent"
+      version            = "current-major"
+      package_state      = "installed"
+      enable_autoupgrade = true
+    },
+  ]
+}
+
 resource "google_logging_project_sink" "instance-sink" {
   depends_on = [google_storage_bucket.cos-logging-bucket]
   name        = "my-instance-sink"
