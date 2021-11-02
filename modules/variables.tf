865a72152c687879d8f909eca0d3773b75e23b6a
amwr91 <amwr91@gmail.com>
2021-11-02 17:33:31 +0100

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,3 +2,9 @@ variable "additional_components" {
   description = "Additional gcloud CLI components to install. Defaults to none. Valid value are components listed in `gcloud components list`"
   default     = []
 }
+
+variable "enabled" {
+  description = "Flag to optionally disable usage of this module."
+  type        = bool
+  default     = true
+}
