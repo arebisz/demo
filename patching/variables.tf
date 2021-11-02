7653420d8c13e9d37d31854fd1f9e951b5e9e0a7
amwr91 <amwr91@gmail.com>
2021-11-02 06:15:48 +0100

    Update variables.tf

--- patching/variables.tf
+++ patching/variables.tf
@@ -1,49 +1,14 @@
-variable "instance_name_prefixes" {
-    type = string
-    description = "Instance name prefix for paching"
-}
-
-variable "duration" {
-    type = string
-    description = "Duration (maintenance window)"
-}
-variable "rollout_mode" {
-    type = string
-    description = "Zone rollout"
-}
-
-variable "rollout_disruption_budget_percentage" {
-    type = number
-    description = "Specify the maximum number or percentage of VM instances per zone that may be disrupted by the patch job"
-}
-
-variable "recurring_schedule" {
-    type = bool
-    description = "Enable recurring schedule"
-}
-
-variable "recurring_schedule_time_zone_id" {
-    type = string
-    description = "Timezone"
-}
-
-variable "recurring_schedule_time_of_day_hours" {
-    type = number
-    description = "Start time hour"
-}
-
-variable "recurring_schedule_time_of_day_minutes" {
-    type = number
-    default = 00
-    description = "Start time minutes"
-}
-
-variable "recurring_schedule_monthly_month_day" {
-    type = number
-    description = "Start time minutes"
-}
-
-variable "one_time_schedule_execute_time" {
-    type = string
-    description = "Patching execute time"
-}
+variable "project_id" {}
+variable "zone" {}
+variable "region" {}
+variable "cos_patching_labels" {}
+variable "instance_name_prefixes" {}
+variable "duration" {}
+variable "rollout_mode" {}
+variable "rollout_disruption_budget_percentage" {}
+variable "recurring_schedule" {}
+variable "recurring_schedule_time_zone_id" {}
+variable "recurring_schedule_time_of_day_hours" {}
+variable "recurring_schedule_time_of_day_minutes" {}
+variable "recurring_schedule_monthly_month_day" {}
+variable "one_time_schedule_execute_time" {}
