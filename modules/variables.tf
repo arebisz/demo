b597b295eb1bd13a10139a6c5dc97837e2e06964
amwr91 <amwr91@gmail.com>
2021-11-02 18:04:07 +0100

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,16 +1,53 @@
-variable "additional_components" {
-  description = "Additional gcloud CLI components to install. Defaults to none. Valid value are components listed in `gcloud components list`"
-  default     = []
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
 }
 
-variable "enabled" {
-  description = "Flag to optionally disable usage of this module."
-  type        = bool
-  default     = true
+variable "policy_id" {
+  description = "The ID of the policy."
+  type        = string
 }
 
-variable "skip_download" {
-  description = "Whether to skip downloading gcloud (assumes gcloud is already available outside the module)"
-  type        = bool
-  default     = true
+variable "description" {
+  description = "The description of the policy."
+  type        = string
+  default     = null
+}
+
+variable "agent_rules" {
+  description = "A list of agent rules to be enforced by the policy."
+  type        = list(any)
+
+  validation {
+    condition     = can([for agent_rule in var.agent_rules : agent_rule["type"]])
+    error_message = "Each agent rule must have a type."
+  }
+}
+
+variable "group_labels" {
+  description = "A list of label maps to filter instances to apply policies on."
+  type        = list(map(string))
+  default     = null
+}
+
+variable "os_types" {
+  description = "A list of OS types to filter instances to apply the policy."
+  type        = list(any)
+
+  validation {
+    condition     = can([for os_type in var.os_types : os_type["short_name"]])
+    error_message = "Each os type must have a short_name."
+  }
+}
+
+variable "zones" {
+  description = "A list of zones to filter instances to apply the policy."
+  type        = list(string)
+  default     = null
+}
+
+variable "instances" {
+  description = "A list of instances to filter instances to apply the policy."
+  type        = list(string)
+  default     = null
 }
