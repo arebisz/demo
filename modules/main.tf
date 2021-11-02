cc8eb3420fd181e26db88891b760d3f4ccbb7048
amwr91 <amwr91@gmail.com>
2021-11-02 16:54:06 +0100

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,9 @@
+locals {
+  components           = join(",", var.additional_components)
+  gcloud              = local.skip_download ? "gcloud" : "${local.gcloud_bin_path}/gcloud"
+  additional_components_command                = "${path.module}/scripts/check_components.sh ${local.gcloud} ${local.components}"
+}
+
 module "agent_policy" {
   source     = "terraform-google-modules/cloud-operations/google//modules/agent-policy"
   version    = "~> 0.1.0"
