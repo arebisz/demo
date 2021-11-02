99e982fc5067cfa68fe5c7d7cfde8d0125f17032
amwr91 <amwr91@gmail.com>
2021-11-02 04:59:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ provider "google" {
 module "monitoring" {
     count = var.monitoring_enabled ? 1 : 0
     source = "./monitoring"
+    email_address = var.email_address
+    email_display_name = var.email_display_name
 }
     
 module "patching" {
