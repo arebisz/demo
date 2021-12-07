3fe94329aa6a529baf1c741dbaabef57fa950bed
amwr91 <amwr91@gmail.com>
2021-12-07 13:14:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "google" {
     project = var.project
 }
 
+resource "google_storage_bucket" "cos_terraform_state" {
+name     = var.bucket_name
+location = var.region
+}
+
 module "monitoring" {
     source = "./monitoring"
     count = var.include_monitoring_module ? 1 : 0
