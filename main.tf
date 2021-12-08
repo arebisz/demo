22f48a68325ad63802babedd007ba054d5a61a95
amwr91 <amwr91@gmail.com>
2021-12-08 07:48:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ provider "google" {
     project = var.project
 }
 
-resource "google_storage_bucket" "cos_terraform_state" {
+resource "google_storage_bucket" "cos_gcs" {
     name     = var.bucket_name
     location = var.region
-    force_destroy = true
 }
