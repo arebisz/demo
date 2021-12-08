53643a78793181d57c1825670898648cfacd9483
amwr91 <amwr91@gmail.com>
2021-12-08 07:58:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ resource "google_storage_bucket" "cos_gcs" {
     name     = var.bucket_name
     location = var.region
 }
+
+module "monitoring" {
+    source = "./monitoring"
+    count = var.include_monitoring_module ? 1 : 0
+    email_address = var.email_address
+}
