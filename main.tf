bc3a182c55376053d0883c2e9ae9055c4a1317c9
amwr91 <amwr91@gmail.com>
2021-12-08 06:46:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,3 @@ resource "google_storage_bucket" "cos_terraform_state" {
     name     = var.bucket_name
     location = var.region
 }
-
-module "monitoring" {
-    source = "./monitoring"
-    count = var.include_monitoring_module ? 1 : 0
-    email_address = var.email_address
-    email_display_name = var.email_display_name
-}
