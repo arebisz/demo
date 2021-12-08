974a000c2ace487f62ecd10e42d0638f80968dfd
amwr91 <amwr91@gmail.com>
2021-12-08 06:49:02 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,16 +9,3 @@ variable "bucket_name" {
 variable "region" {
     type =  string
 }
-
-variable "email_address" {
-    type = string 
-}
-
-variable "email_display_name" {
-    type = string 
-}
-
-variable "include_monitoring_module" {
-    type = bool
-    default = false
-}
