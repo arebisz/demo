e1774c56221102f7c0a6d370eb07ac9302a7e987
amwr91 <amwr91@gmail.com>
2021-12-08 08:25:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "cos_gcs" {
 }
 
 module "monitoring" {
-    source = "modules/monitoring"
+    source = "./modules/monitoring"
     count  = var.include_monitoring_module ? 1 : 0
     email_address = var.email_address
 }
