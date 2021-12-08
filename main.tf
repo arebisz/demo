95439089050f7a88252cb7588448bda1bd7e55af
amwr91 <amwr91@gmail.com>
2021-12-08 07:45:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-    required_providers {
-        google = {
-            source = "hashicorp/google"
-        }
-    }
-} 
-
 provider "google" {
     project = var.project
 }
@@ -14,7 +6,4 @@ resource "google_storage_bucket" "cos_terraform_state" {
     name     = var.bucket_name
     location = var.region
     force_destroy = true
-    versioning {
-      enabled = true
-    }
 }
